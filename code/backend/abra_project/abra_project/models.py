55a88b074fc64df56a64a46fb2898b8c06a3dd38
Tamir Banay <banay9329@gmail.com>
2024-02-14 15:13:33 +0200

    careate massage model

--- code/backend/abra_project/abra_project/models.py
+++ code/backend/abra_project/abra_project/models.py
@@ -1,1 +1,12 @@
- 
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Massage(models.Model):
+    MassageId = models.AutoField(primary_key=True)
+    Sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ReceiverMail = models.CharField(max_length=200,  null=False, blank=True)
+    Message= models.CharField(max_length=200,  null=True, blank=True)
+    Subject= models.CharField(max_length=100,  null=True, blank=True)
+    CreationDate=  models.DateTimeField(default=timezone.now)
+    
--- code/backend/abra_project/users/views.py
+++ code/backend/abra_project/users/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login,logout
-from django.contrib import messages
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
