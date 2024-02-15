2dc5af40a81569a5ea86be83fcedcaca16b580e8
Tamir Banay <banay9329@gmail.com>
2024-02-15 18:08:08 +0200

    start new deploy

--- abra_project/abra_project/settings.py
+++ abra_project/abra_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-d%311m7*=@yi5t(8e+%$22%at)b)plcll7t5hj#!l)!c-hw9@r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['abraproject.heroku.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -127,8 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
